2df594d24d4b1e720bc32fcf8c34a7b307e08e3c
seniya <seniyathri@gmail.com>
2024-03-27 21:50:25 +0100

    feat: checks pushed

---
+++ MILESTONE 2/app.js
@@ -0,0 +1,20 @@
+console.log("hello-world");
+
+//create 3 promt dove inserici il nome , cognome, eta del studente
+
+let studenti = [];
+// console.log(studenti);
+//aggiungere il nuovo array di stidente dentro studenti
+
+for (let i = 0; i < studenti.length; i++) {
+  let nomeStudente = prompt("inserisci il tuo nome");
+  let cognomeStudente = prompt("inserisci il tuo cognome");
+  let etaStudente = prompt("inserisci l'eta");
+
+  let counter = studenti.push(nomeStudente);
+
+  // console.log(nomeStudente, cognomeStudente, etaStudente);
+}
+//stampar le proprieta di studenti co il ciclo for
+
+//stampare le proprieta di studenti con il do while
